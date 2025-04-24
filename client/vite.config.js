4555433763f3fb36a6f886af6ed9a03c4a2a615d
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-04-24 16:26:57 +0530

    changed target

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Your Express server URL
+        target: 'https://navil-spinnings-consultancy-project.onrender.com', // Your Express server URL
         changeOrigin: true,
         secure: false
       }
